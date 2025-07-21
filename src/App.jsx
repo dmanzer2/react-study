1c01ffb5cef3d58ae6cba111f63c3720d1166706
David Manzer <dmanzer2@gmail.com>
2025-07-21 14:14:35 -0700

    now published

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import JSXExamples from './components/JSXExamples'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>React Study Guide</h1>
+        <p>A comprehensive guide to learning React</p>
+      </header>
+      <main>
+        <JSXExamples />
+      </main>
+    </div>
+  )
+}
+
+export default App
